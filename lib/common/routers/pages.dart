535c00f3e9fa7ee90efd3728aaa6a818710237d4
Alant <jiexunshen@gmail.com>
2025-06-14 16:19:35 +0800

    init

---
+++ lib/common/routers/pages.dart
@@ -0,0 +1,14 @@
+
+import 'package:get/get.dart';
+
+import '../../pages/index.dart';
+import 'names.dart';
+
+class RoutePages {
+  static List<GetPage> list = [
+      GetPage(
+        name: RouteNames.login,
+        page: () => const LoginPage(),
+      ),
+  ];
+}
